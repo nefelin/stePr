344e4c42b52500aa21bd5f2170537fa3654bbe9b
Eric Lindgren <nephilim.eric@gmail.com>
2017-09-29 12:42:14 -0400

    Added some ideas about how to handing flat representation of pitch, not implemented yet

--- stePr.js
+++ stePr.js
@@ -1,39 +1,39 @@
-//FrameLib stuff
-var ACT = { //I think maybe actions should have no targets
-					// and target should be inherent in acttype, like MOVE_FREE
-					// or SHIFT_WEIGHT
-	ROT_FOOT : "rotate one foot",
-	ROT_BOD  : "rotate both feet around standing",
-	MOVE     : "translate foot/weight in XY space",
-	POINT    : "articulate the ankle"
-}
+// //FrameLib stuff
+// var ACT = { //I think maybe actions should have no targets
+// 					// and target should be inherent in acttype, like MOVE_FREE
+// 					// or SHIFT_WEIGHT
+// 	ROT_FOOT : "rotate one foot",
+// 	ROT_BOD  : "rotate both feet around standing",
+// 	MOVE     : "translate foot/weight in XY space",
+// 	POINT    : "articulate the ankle"
+// }
 
-function Action(type, amount, target){
-	if (true){ //want to test valid act_type but enum is weird in JS BUG*
-		this.type = type;
-		this.amount = amount;
-		this.target = target;
-	}
-	else {console.log('ERROR ', type);}
-}
-
-
-function Frame(actions, absolutes) {
-	this.actions = actions; //Should be an array of Actions
-	this.absolutes = absolutes; //This should be an object containing absolute 
-								//position/rotation/yaw of each foot
-}
-
-function FrameData(xyPos, yawAngle, pitchAngle){
-	this.xyPos = xyPos;
-	this.yawAngle = yawAngle;
-	this.pitchAngle = pitchAngle;
-}
+// function Action(type, amount, target){
+// 	if (true){ //want to test valid act_type but enum is weird in JS BUG*
+// 		this.type = type;
+// 		this.amount = amount;
+// 		this.target = target;
+// 	}
+// 	else {console.log('ERROR ', type);}
+// }
 
 
-function FrameLib(){
-	this.frames = {};
-}
+// function Frame(actions, absolutes) {
+// 	this.actions = actions; //Should be an array of Actions
+// 	this.absolutes = absolutes; //This should be an object containing absolute 
+// 								//position/rotation/yaw of each foot
+// }
+
+// function FrameData(xyPos, yawAngle, pitchAngle){
+// 	this.xyPos = xyPos;
+// 	this.yawAngle = yawAngle;
+// 	this.pitchAngle = pitchAngle;
+// }
+
+
+// function FrameLib(){
+// 	this.frames = {};
+// }
 
 //View CLASS Should contain objects, track/set frame number, facilitating animation
 //Not sure where to keep key frame data, with dancer or individual objects
@@ -46,10 +46,12 @@ function ViewController(frameCount, objects){
 	this.FRAMECOUNT = typeof frameCount == 'undefined' ? 20 : frameCount;
 
 	if (objects == undefined) this.objects = [];
-	else this.objects = {};
+	else this.objects = [];
 
 	this.currentFrame = 0; 
-	this.allFrames = [];
+	this.uiFrameBoxes = [];
+
+	//Init FrameLib
 	this.frameLib = {};
 
 	this.setFrame = function (frameNum) {
@@ -59,10 +61,22 @@ function ViewController(frameCount, objects){
 		
 	}.bind(this);
 
-	this.logFrame = function() { //Hobbled to work with one object and only xy pos
+	this.logFrame = function() { 
 		console.log('log!');
+
+		// if (!(this.currentFrame in this.frameLib)){ //If there's no keyframe make a new blank. 
+		if (!(this.currentFrame in this.frameLib)){
+			this.frameLib[this.currentFrame] = {}
+		}
+
 		for (objI in this.objects){
-			this.frameLib[this.currentFrame][objects][this.objects[objI]]['position'] = this.objects[objI].position;
+			this.frameLib[this.currentFrame][this.objects[objI].id] = {}
+			var thisEntry = this.frameLib[this.currentFrame][this.objects[objI].id];
+			var thisObject = this.objects[objI];
+			thisEntry.position = thisObject.position;
+			thisEntry.yaw =  thisObject.rotation;
+			console.log(thisEntry.position);
+
 		}
 		this.updateUI();//make sure frames show new content
 
@@ -103,7 +117,7 @@ function ViewController(frameCount, objects){
 				}
 			})(i, frame, this);
 
-			this.allFrames.push(frame);
+			this.uiFrameBoxes.push(frame);
 		}
 		//Frame Count
 		
@@ -118,17 +132,17 @@ function ViewController(frameCount, objects){
 	}.bind(this);
 
 	this.updateUI = function(){
-		for (var i in this.allFrames){ //Update frame boxes
+		for (var i in this.uiFrameBoxes){ //Update frame boxes
 			// console.log('updating frame ' + i);
 			
-			if (i == this.currentFrame) this.allFrames[i].strokeColor = UICOLORS.frameStrokeSelected;
-			else this.allFrames[i].strokeColor = UICOLORS.frameStrokeUnSelected;
+			if (i == this.currentFrame) this.uiFrameBoxes[i].strokeColor = UICOLORS.frameStrokeSelected;
+			else this.uiFrameBoxes[i].strokeColor = UICOLORS.frameStrokeUnSelected;
 
 			if (i in this.frameLib){
-					this.allFrames[i].fillColor = 'black';	
+					this.uiFrameBoxes[i].fillColor = 'black';	
 				}
 				else {
-					this.allFrames[i].fillColor = 'grey';
+					this.uiFrameBoxes[i].fillColor = 'grey';
 				}
 		};
 
@@ -136,12 +150,14 @@ function ViewController(frameCount, objects){
 	}.bind(this);
 
 	this.updateContents = function(){
-		// if (this.currentFrame in this.frameLib) {
-		// 	for (var obIndex in this.frameLib[this.currentFrame].objects){
-				
-		// 	}
-		// }
-		console.log(this.frameLib);
+		if (this.currentFrame in this.frameLib){
+			for (obIndex in this.objects){
+				var thisOb = this.objects[obIndex];
+				thisOb.position = this.frameLib[this.currentFrame][thisOb.id].position;
+				console.log("Setting object #: " + thisOb.id + " to position: " + this.frameLib[this.currentFrame][thisOb.id].position);
+			}
+		}
+		// console.log(this.frameLib);
 	}.bind(this);
 
 	//INITIALIZE INTERFACE
@@ -154,15 +170,51 @@ function ViewController(frameCount, objects){
 
 //Test ActionLog
 
-var testAct = new Action(ACT.MOVE,2,3);
-var testFrame = new Frame([testAct],'absolute');
-var testLib = {};
-testLib[5] = testFrame;
+// var testAct = new Action(ACT.MOVE,2,3);
+// var testFrame = new Frame([testAct],'absolute');
+// var testLib = {};
+// testLib[5] = testFrame;
 
 // console.log(test);
 
 
+//Proto Foot Class to allow yaw shading to indicate footsteps, still need to figure out how to hand when rotated out of cardinal directions...
 
+
+// function Foot(position){
+// 	this.yaw = 0;
+// 	this.shadowRep = new paper.Path.Rectangle(view.center, [20,60])
+// 	this.footRep = shadowRep.clone();
+// 	this.shadowRep.opacity = .1;
+
+// }
+
+// Object.defineProperty(Foot, 'position', {
+// 	get: function() {
+// 		return this.footRep.position;
+// 	},
+// 	set: function(value) {
+// 		this.footRep.position = value;
+// 		this.shadowRep.position = value;
+// 	}
+// });
+
+// Object.defineProperty(Foot, 'yaw', {
+// 	get: function() {
+// 		return this.yaw;
+// 	},
+// 	set: function(value) {
+// 		this.yaw = value;
+// 		if (this.yaw == 0) {//Mostly feet will be flat
+// 		}
+// 		else if (this.yaw<0){//When not flat likely to tilt back
+// 		}
+// 		else if (this.yaw>0){//But somtimes they're pointed forward
+// 		}
+
+
+// 	}
+// });
 
 var button = new Path.Rectangle([10,10], [100,40]);
 button.fillColor = 'red';
@@ -176,9 +228,10 @@ button.onClick = function(event){
 var mainView = new ViewController();
 
 
-var obj1 = new paper.Path.Circle(view.center, 50)
+var obj1 = new paper.Path.Rectangle(view.center, [50,150])
 obj1.fillColor = 'black';
 console.log('object id:' + obj1.id);
+mainView.objects.push(obj1);
 
 
 
