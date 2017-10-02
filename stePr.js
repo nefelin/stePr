79678be7b9eaa036b24a627a08c558e309a7aeae
Eric Lindgren <nephilim.eric@gmail.com>
2017-10-02 15:17:21 -0400

    Tons of changes, mostly focused around individual translation tracking, still only partially implemented

--- stePr.js
+++ stePr.js
@@ -1,12 +1,5 @@
 // //FrameLib stuff
-// var ACT = { //I think maybe actions should have no targets
-// 					// and target should be inherent in acttype, like MOVE_FREE
-// 					// or SHIFT_WEIGHT
-// 	ROT_FOOT : "rotate one foot",
-// 	ROT_BOD  : "rotate both feet around standing",
-// 	MOVE     : "translate foot/weight in XY space",
-// 	POINT    : "articulate the ankle"
-// }
+
 
 // function Action(type, amount, target){
 // 	if (true){ //want to test valid act_type but enum is weird in JS BUG*
@@ -37,14 +30,43 @@
 
 //View CLASS Should contain objects, track/set frame number, facilitating animation
 //Not sure where to keep key frame data, with dancer or individual objects
+
+var ACT_TYPES = { //I think maybe actions should have no targets
+					// and target should be inherent in acttype, like MOVE_FREE
+					// or SHIFT_WEIGHT
+	ROT_FOOT : "rotate one foot",
+	// ROT_BOD  : "rotate both feet around standing",
+	MOVE     : "translate foot/weight in XY space",
+	// POINT    : "articulate the ankle"
+}
+
+var UISYMBOLS = {
+	ACT_MOVE: new Path.Rectangle({
+		point: [-100,-100],
+		size: [15,15],
+		fillColor: 'black',
+	}),
+	ACT_ROTATE: new Path.Circle({
+		center: [-100,-100],
+		radius: 7.5,
+		fillColor: 'blue'
+	}),
+}
+
+
 var UICOLORS = {
 	frameStrokeSelected : 'blue',
 	frameStrokeUnSelected : 'black'
 }
 
 function ViewController(frameCount, objects, containerView){
+	this.UIGroupHUD = new Group();
+	this.UIGroupActionLog = new Group();
 
-
+	//Vars for recording individual translations
+	this.obStartState = null; //Used to track object change
+	this.actionLog = {}; //Holds current translations until they are logged to a keyframe
+	this.actionLogUI = [];
 
 	this.focusedObj = null;
 	this.userAction = 'none';
@@ -65,7 +87,7 @@ function ViewController(frameCount, objects, containerView){
 
 	this.addObj = function(obToAdd){
 		
-		// obToAdd.applyMatrix = false; //It's important that all objects in view don't apply matrix but we'll handly this in object design rather than having the view enforce it
+		obToAdd.applyMatrix = false; //TODO It's important that all objects in view don't apply matrix but we'll handly this in object design rather than having the view enforce it
 
 		//Add event handlers for object focus
 		obToAdd.onMouseEnter = function(event) {
@@ -86,15 +108,18 @@ function ViewController(frameCount, objects, containerView){
 	}.bind(this);
 
 	this.setFrame = function(frameNum){
-		console.log(frameNum);
+		
 		if ((frameNum < 0) || (frameNum >= this.FRAMECOUNT)) return -1;
 		this.currentFrame = frameNum;
-		this.updateContents();
+		this.loadFrame();
 		this.updateUI();
 		
 	}.bind(this);
 
 	this.logFrame = function(){
+		if (this.userAction != 'none'){
+			this.endAction();
+		}
 
 		// if (!(this.currentFrame in this.frameLib)){ //If there's no keyframe make a new blank. 
 		if (!(this.currentFrame in this.frameLib)){
@@ -105,22 +130,57 @@ function ViewController(frameCount, objects, containerView){
 			this.frameLib[this.currentFrame][this.objects[objI].id] = {}
 			var thisEntry = this.frameLib[this.currentFrame][this.objects[objI].id];
 			var thisObject = this.objects[objI];
+
 			thisEntry.position = thisObject.position;
-			thisEntry.yaw =  thisObject.rotation;
-			// console.log("Entered yaw " + thisEntry.yaw);
+			thisEntry.rotation =  thisObject.rotation;
+
+			thisEntry.actionLog = this.actionLog;
 
 		}
 		this.updateUI();//make sure frames show new content
 
 	}.bind(this);
 
+	this.loadFrame = function(){
+		this.actionLog = {}; //Clear leftovers from last frame;
+		this.UIGroupActionLog.removeChildren();
+		// for (var i in this.UILayerActions.){
+		// 	this.actionLogUI[i].remove();
+		// }
+
+		if (this.currentFrame in this.frameLib){
+			for (obIndex in this.objects){
+				var thisOb = this.objects[obIndex];
+				var thisEntry = this.frameLib[this.currentFrame][thisOb.id];
+
+
+				thisOb.position = thisEntry.position; //Would be nice to set this to a function rather than recording each property
+				thisOb.rotation = thisEntry.rotation;	  //so that if we add or remove properties they can be stored or loaded programattically
+
+				this.actionLog = thisEntry.actionLog;
+				// console.log("Setting object #: " + thisOb.id + " to position: " + thisEntry.position + "\nSetting rotation to: " + thisEntry.rotation);
+			}
+		}
+		else {
+			
+		}//Interpolate
+
+
+		 // console.log(this.frameLib);
+	}.bind(this);
+
 	this.initUI = function(){
-		// console.log('initing UI');
+		//Buttons
+		var buttonRect = new Rectangle(view.center,[100,40])
+		var button1 = new Button(buttonRect, 'blue', 'Play', function(){alert('Will run animation')});
+
+		var button2 = new Button(buttonRect, 'red', 'log', function(event){this.logFrame();});
+		button1.position = [60,30] //TODO (location should be set in constructor maybe change the rect parameter)
+		button2.position = [60,80]
 
 
 
 		//Create FrameUI View
-		// console.log(this.FRAMECOUNT);
 		for (var i = 0; i<this.FRAMECOUNT;i++){
 		 
 			var frame = new Path.Rectangle([10+15*i,550], [10,40]);
@@ -142,7 +202,6 @@ function ViewController(frameCount, objects, containerView){
 				whichframe.idNum = ind;
 				// alert(ind);
 				whichframe.onClick = function(event){
-					// console.log('frameNum: ' + whichframe.idNum + ' clicked');
 					controller.setFrame(ind);
 				}
 			})(i, frame, this);
@@ -150,7 +209,6 @@ function ViewController(frameCount, objects, containerView){
 			this.uiFrameBoxes.push(frame);
 		}
 		//Frame Count
-		
 		this.frameText = new PointText({
 		    point: [view.bounds.width-200, 50],
 		    content: 'Frame: \n Action: ',
@@ -177,27 +235,66 @@ function ViewController(frameCount, objects, containerView){
 		};
 
 		this.frameText.content = 'Frame #: ' + this.currentFrame + "\nAction: " + this.userAction;
-		console.log('UI Updated');
-		// console.log(this.frameText.content);
-	}.bind(this);
 
-	this.updateContents = function(){
-		if (this.currentFrame in this.frameLib){
-			for (obIndex in this.objects){
-				var thisOb = this.objects[obIndex];
-				var thisEntry = this.frameLib[this.currentFrame][thisOb.id];
+		//DraUILayerObject
+		
 
+		this.UIGroupActionLog.removeChildren()
+		// this.drawActionLog();
 
-				thisOb.position = thisEntry.position;
-				thisOb.rotation = thisEntry.yaw;
-				// console.log("Setting object #: " + thisOb.id + " to position: " + thisEntry.position + "\nSetting rotation to: " + thisEntry.yaw);
+		var itemCount = 0;
+		for (key in this.actionLog) {
+
+			itemCount++;
+			switch (key){
+				case 'position': //make boxes for 
+					var logItemRep = UISYMBOLS.ACT_MOVE.clone();
+					break;
+				case 'rotation':
+					var logItemRep = UISYMBOLS.ACT_ROTATE.clone();
+					break;
 			}
+
+			(function(thisKey, thisItem, thisLog, thisContext) {
+				logItemRep.onClick = function(){
+					// alert(thisKey)
+
+					thisItem.remove();
+
+					delete thisLog[thisKey];
+
+					console.log(thisLog);
+				}
+
+			;})(key, logItemRep, this.actionLog);
+
+
+			// (function(item, thisKey, log) {
+			// 	logItemRep.onClick = function(){
+			// 		console.log('transform click');
+			// 		console.log(log.thisKey);
+			// 		// item[thisKey] -= log.thisKey; //reverse movement on object, 
+
+			// 		delete log.thisKey;//delete key
+
+					
+			// 		logItemRep.remove();//delete rep
+			// 	}
+
+			// }.bind(this))(logItemRep, key, this.actionLog)
+
+			logItemRep.position = [view.bounds.width-200,100+itemCount*20];
+			this.UIGroupActionLog.addChild(logItemRep);
+
 		}
-		else {}//Interpolate
 
 
-		// console.log(this.frameLib);
+		// console.log("active: " + project.activeLayer);
+		// console.log("all layers " + project.layers)
+
 	}.bind(this);
+
+	
 
 	//INITIALIZE INTERFACE
 	this.initUI();
@@ -205,23 +302,69 @@ function ViewController(frameCount, objects, containerView){
 
 	//User Interaction Stuff:
 	this.setAction = function(newAction){
-		if (this.userAction == newAction) this.userAction = 'none';
-		else {
-			this.userAction = newAction;
+		if (this.focusedObj!=null){
+			if (this.userAction == newAction) {
+				this.endAction();
+				this.userAction = 'none';
+			}
+			else {
+				this.startAction(newAction);
+				this.userAction = newAction;
+			}
+			this.updateUI();
 		}
+	}.bind(this);
+
+	this.startAction = function(actType){
+
+		switch (actType){//record targets current state (only for relevant action)
+			case 'position':
+				this.obStartState = this.focusedObj.position;
+				break;
+			case 'rotation':
+				this.obStartState= this.focusedObj.rotation;
+				break;
+			case 'pitch':
+				this.obStartState= this.focusedObj.pitch;
+				break;
+		}
+		// console.log(this.obStartState);
+
+		//set the the action state, does that need to be separate function?
+	}.bind(this);
+
+	this.endAction = function(){
+		switch (this.userAction){//Note difference between last position and this position (or rotation or pitch or whatever)
+			case 'position':
+				var stateDiff = this.obStartState - this.focusedObj.position;
+				break;
+			case 'rotation':
+				var stateDiff = this.obStartState - this.focusedObj.rotation;
+				break;
+			case 'pitch':
+				var stateDiff = this.obStartState - this.focusedObj.pitch;
+				break;
+		}
+		
+		this.actionLog[this.userAction] = stateDiff;//Record Difference record movement in the actionLog for this frame, which will be added (plus absolute positions) to the framelib object IF the frame gets logged
+		// console.log(this.actionLog);
+		
+
+		this.updateUI();//update the UI to show new actions created
 	}.bind(this);
 
 	containerView.onKeyDown = function(event){
 		// console.log(event.key + 'pressed');
 		switch(event.key){
 			case 'g':
-				this.setAction('move');
+				this.setAction('position');
 				break;
 			case 'r':
-				this.setAction('rotate');
+				this.setAction('rotation');
 				break;
 			case 'space':
-				this.logFrame();
+				this.logFrame(); 
+				this.setFrame(this.currentFrame+1);
 				break;
 			case 'f':
 				this.setFrame(this.currentFrame+1);
@@ -230,24 +373,20 @@ function ViewController(frameCount, objects, containerView){
 				this.setFrame(this.currentFrame-1);
 				break;
 		}
-		this.updateUI();
+		
 	}.bind(this);
 
 	containerView.onMouseMove = function(event){
 		switch (this.userAction){
-			case 'move':
+			case 'position':
 				this.focusedObj.position += event.delta;
 				break;
-			case 'rotate':
+			case 'rotation':
 				this.focusedObj.rotation += event.delta.y;
 				break;
 
 		}
 	}.bind(this);
-
-	containerView.onMouseClick = function(event){
-		this.setAction('none');
-	}
 
 }
 
@@ -319,6 +458,18 @@ function Foot(position){ //FOOT class needs to extend paper.item otherwise it ca
 	set: function(value) {
 		this.shadowRep.selected = value;
 		console.log('foot selected');
+	}
+});
+
+
+//Getter/Setter Methods
+
+Object.defineProperty(this, 'onMouseEnter', {
+	get: function() {
+		return this.footGroup.onMouseEnter;
+	},
+	set: function(value) {
+		this.footGroup.onMouseEnter = value;
 	}
 });
 
@@ -425,27 +576,21 @@ Object.defineProperty(this, 'yaw', { //The problem is with these setters every t
 //Create View	
 var mainView = new ViewController(50, null, view);
 
-//Create Buttons
-var buttonRect = new Rectangle(view.center,[100,40])
-var button1 = new Button(buttonRect, 'blue', 'Test', function(){alert('test')});
 
-var button2 = new Button(buttonRect, 'red', 'log', function(event){mainView.logFrame();});
 
-button1.position = [60,30] //TODO (location should be set in constructor maybe change the rect parameter)
-button2.position = [60,80]
+
 
 
 //Create stuff to animate:
 var obj1 = new paper.Path.Rectangle(view.center, [50,150])
 obj1.fillColor = 'black';
 
-var testFoot = new Foot();
+// var testFoot = new Foot(); //OUT until we fix foot specific code TODO
+// mainView.addObj(testFoot);
 
 
-
-//Add stuff to scene
 mainView.addObj(obj1);
-mainView.addObj(testFoot);
+
 
 // onMouseMove = function(event){
 // 	obj1.position += event.delta;
